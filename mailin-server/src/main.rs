51144809aadeafdb6c0c34f919482286acd5a0b9
Alienscience <saul@alienscience.org.uk>
2019-03-09 08:53:05 +0100

    Temporary deploy of blocklist only

--- mailin-server/src/main.rs
+++ mailin-server/src/main.rs
@@ -34,6 +34,7 @@ struct Handler {
 impl mailin_embedded::Handler for Handler {
     fn helo(&mut self, ip: IpAddr, _domain: &str) -> HeloResult {
         // Does the reverse DNS match the forward dns?
+        /*
         let rdns = self.mxdns.fcrdns(ip);
         match rdns {
             Ok(ref res) if !res.is_confirmed() => HeloResult::BadHelo,
@@ -44,6 +45,12 @@ impl mailin_embedded::Handler for Handler {
                     HeloResult::Ok
                 }
             }
+        }
+        */
+        if self.mxdns.is_blocked(ip).unwrap_or(false) {
+            HeloResult::BlockedIp
+        } else {
+            HeloResult::Ok
         }
     }
 }
