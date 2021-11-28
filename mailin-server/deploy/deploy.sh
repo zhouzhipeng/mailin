8bd01827cac68979b006c2d07153fae10c901e00
Alienscience <saul@alienscience.org.uk>
2021-11-28 12:08:30 +0100

    Checksum before deploy

--- mailin-server/deploy/deploy.sh
+++ mailin-server/deploy/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-DEPLOY_COMPLETE_FILE=$HOME/rpmbuild/RPMS/commit
+set -e
+
 RPM_DIR=$HOME/rpmbuild/RPMS/x86_64/
 
-# Use an empty file to signal that the deploy is complete.
-touch $DEPLOY_COMPLETE_FILE
+# Use a checksum file to signal that the deploy is complete.
+find "$RPM_DIR" -name '*.rpm' -print0 | \
+     xargs -0 -L1 bash -c 'sha256sum "$0" > "$0.sha256sum"'
 
 sftp -P ${SSH_DEPLOY_PORT} -o "StrictHostKeyChecking=no" \
      ${SSH_DEPLOY_USER}@mail.spamtastic.cc:inbox/ << EOF
 put -f $RPM_DIR/*.rpm
-put -f $DEPLOY_COMPLETE_FILE
+put -f $RPM_DIR/*.sha256sum
 ls -lh
 EOF
