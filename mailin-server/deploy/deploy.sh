521f4899af6eacfa95d2d65db5f3668455ca293a
Alienscience <saul@alienscience.org.uk>
2022-01-15 09:42:54 +0100

    Deploy from tar file

--- mailin-server/deploy/deploy.sh
+++ mailin-server/deploy/deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
-
 set -e
 
-RPM_DIR=$HOME/rpmbuild/RPMS/x86_64/
+mkdir tmp-deploy
+cp mailin-server/deploy/install tmp-deploy/
+cp mailin-server/deploy/mailin.service tmp-deploy/
+cp target/release/mailin-server tmp-deploy/mailin
+cd tmp-deploy
+tar --zstd -cf mailin.tar.zst install mailin 
 
 # Use a checksum file to signal that the deploy is complete.
-find "$RPM_DIR" -name '*.rpm' -print0 | \
-     xargs -0 -L1 bash -c 'sha256sum "$0" > "$0.sha256sum"'
+sha256sum "mailin.tar.zst" > "mailin.tar.zst.sha256sum"
 
-sftp -P ${SSH_DEPLOY_PORT} -o "StrictHostKeyChecking=no" \
-     ${SSH_DEPLOY_USER}@mail.spamtastic.cc:inbox/ << EOF
-put -f $RPM_DIR/*.rpm
-put -f $RPM_DIR/*.sha256sum
+sftp -P "${SSH_DEPLOY_PORT}" -o "StrictHostKeyChecking=no" \
+     "${SSH_DEPLOY_USER}"@mail.spamtastic.cc:inbox/ << EOF
+put -f *.tar.zst
+put -f *.sha256sum
 ls -lh
 EOF
--- mailin-server/deploy/rpmbuild/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ~/rpmbuild/SPECS
-ln -f -s ${CI_PROJECT_DIR}/mailin-server/deploy/rpmbuild/mailin.spec
-rpmbuild --target x86_64 -bb mailin.spec
